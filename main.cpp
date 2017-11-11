3ffa79922c8faf2fd16f06da4375797a525d41db
Jan Jongboom <janjongboom@gmail.com>
2017-11-11 17:07:30 +0900

    Add LICENSE, fix indentation

--- main.cpp
+++ main.cpp
@@ -9,50 +9,52 @@ UDPSocket socket;
 // Receive data from the server
 void receiveUDP() {
     // Allocate 2K of data
-    void* data = malloc(2048);
+    char* data = (char*)malloc(2048);
     while (1) {
         // recvfrom blocks until there is data
         nsapi_size_or_error_t size = socket.recvfrom(NULL, data, 2048);
-        if (size < 0) {
-            // printf("Error while receiving data from UDP socket (%d)\r\n", size);
+        if (size <= 0) {
+            if (size == NSAPI_ERROR_WOULD_BLOCK) continue; // OK... this is a non-blocking socket and there's no data on the line
+
+            printf("Error while receiving data from UDP socket (%d)\n", size);
             continue;
-		}
+        }
 
-		// turn into valid C string
-		data[size] = '\0';
+        // turn into valid C string
+        data[size] = '\0';
 
-        printf("Received %d bytes from UDP socket (%s)\r\n", size, data);
+        printf("Received %d bytes from UDP socket (%s)\n", size, data);
     }
 }
 
 int main() {
-	printf("Welcome to Mbed UDP ping pong\n");
+    printf("Welcome to Mbed UDP ping pong\n");
 
-	NetworkInterface *network = easy_connect(true);
-	if (!network) {
-		printf("Failed to get a network connection... See serial output.\n");
-		return 1;
-	}
+    NetworkInterface *network = easy_connect(true);
+    if (!network) {
+        printf("Failed to get a network connection... See serial output.\n");
+        return 1;
+    }
 
     // Open a UDP socket
     nsapi_error_t rt = socket.open(network);
     if (rt != NSAPI_ERROR_OK) {
-        printf("Could not open UDP Socket (%d)\r\n", rt);
+        printf("Could not open UDP Socket (%d)\n", rt);
         return 1;
     }
 
-	socket.set_blocking(false);
+    socket.set_blocking(false);
 
-	Thread socket_thread;
-	socket_thread.start(&receiveUDP);
+    Thread socket_thread;
+    socket_thread.start(&receiveUDP);
 
-	while (1) {
-		char buffer[] = "ping";
-		socket.sendto(HOST_IP, HOST_PORT, buffer, strlen(buffer));
+    while (1) {
+        char buffer[] = "ping";
+        socket.sendto(HOST_IP, HOST_PORT, buffer, strlen(buffer));
 
-		printf("sent ping\n");
-		wait_ms(2000);
-	}
+        printf("Sent %d bytes over UDP socket (%s)\n", strlen(buffer), buffer);
+        wait_ms(2000);
+    }
 
-	wait_ms(osWaitForever);
+    wait_ms(osWaitForever);
 }
